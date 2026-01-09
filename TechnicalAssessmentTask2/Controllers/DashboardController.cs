9765bfbf3d7d6500523c2407e2546066c97f1150
hammad <hammad@DESKTOP-OFI49S6>
2026-01-09 18:00:37 +0500

    Add project files.

---
+++ TechnicalAssessmentTask2/Controllers/DashboardController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechnicalAssessmentTask2.Controllers
+{
+    [AllowAnonymous]
+    public class DashboardController : Controller
+    {
+        public IActionResult CommentsReport()
+        {
+            return View();
+        }
+        public IActionResult HeatMap()
+        {
+            return View();
+        }
+        public IActionResult PriorityMatrix()
+        {
+            return View();
+        }
+    }
+}
